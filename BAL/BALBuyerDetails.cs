64fa69d31b74445995479ea40bdd46d61b3de16c
ssm-12 <souvikroy.it@gmail.com>
2017-01-20 21:39:24 +0530

    #Saksham - Commiting BALBuyerDetails.cs and BOBuyerDetails.cs

---
+++ BAL/BALBuyerDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+using DAL;
+
+namespace BAL
+{
+    public class BALBuyerDetails
+    {
+        DALBuyerDetails DAbuyerDL=new DALBuyerDetails();
+
+        public int SaveBuyerDetails(BOBuyerDetails BObuyerDL)
+        {
+          //  return DAbuyerDL.SaveBuyerDetails(BObuyerDL);
+            return 0;
+        }
+    }
+}
---
+++ BO/BOBuyerDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class BOBuyerDetails
+    {
+        public string buyerName { get; set; }
+        public string buyerContactNo { get; set; }
+        public string buyerAddr1 { get; set; }
+        public string buyerAddr2 { get; set; }
+        public string buyerLlNo { get; set; }
+        public string buyerTinNo { get; set; }
+        public string buyerEmailId { get; set; }
+    }
+}
